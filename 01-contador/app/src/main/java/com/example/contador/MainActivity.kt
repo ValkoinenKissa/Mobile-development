fd7a22831af1bb062ad875e9664cb02392df7f4c
Alberto Bollo <albertobollo7@protonmail.com>
2025-10-15 10:44:10 +0200

    Fixed error: counter lost when rotating screen

--- 01-contador/app/src/main/java/com/example/contador/MainActivity.kt
+++ 01-contador/app/src/main/java/com/example/contador/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.contador
+
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
@@ -8,9 +10,12 @@ class MainActivity : ComponentActivity() {
 
     var contador = 0
 
+    val tag = "ESTADOS_CONTADOR"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)  // Esto PRIMERO
+        Log.d(tag, "En el metodo onCreate")
 
         // Referencia al TextView
         val textViewContador = findViewById<TextView>(R.id.textViewContador)
@@ -26,5 +31,56 @@ class MainActivity : ComponentActivity() {
             contador++
             textViewContador.text = contador.toString()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(tag, "En el metodo onStar")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(tag, "En el metodo onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(tag, "En el metodo onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(tag, "En el metodo onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(tag, "En el metodo onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(tag, "En el metodo onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(
+            tag, "onSaveInstanceState, guardado contador con valor " +
+                    contador.toString()
+        )
+        outState.putInt("CONTADOR", contador)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        contador = savedInstanceState.getInt("CONTADOR")
+        Log.d(
+            tag, "onSaveInstanceState, restauro el contador con valor " +
+                    contador.toString()
+        )
+
+        val textViewContador: TextView = findViewById(R.id.textViewContador)
+        textViewContador.text = contador.toString()
     }
 }
