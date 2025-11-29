f393f95444ec670d19976d3cba201bc257aba9f6
Alberto Bollo <albertobollo7@protonmail.com>
2025-11-29 06:22:16 +0100

    misc: updated gradle dependencies

--- 03-calculadora/app/build.gradle.kts
+++ 03-calculadora/app/build.gradle.kts
@@ -50,4 +50,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //Dependencia para el parser matematico
+    implementation(libs.mathparser.org.mxparser)
 }
